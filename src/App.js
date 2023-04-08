538820ad399fa6e5c510dfddd938519d3f3108c6
Damon Allen <damon.nicholas.allen@live.com>
2023-04-08 00:26:33 -0400

    styled, working, and pulling proper data!

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+import Character from './components/Character';
+import Details from './components/Details';
+
+const StyledApp = styled.div`
+  font-family: sans-serif;
+`
+const StyledHeader = styled.h1`
+  font-size: 3rem;
+`
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [data, setData] = useState([]);
+  console.log(data);
+  const [currentId, setCurrentId] = useState('');
 
+
+  const opener = id => {
+    setCurrentId(id + 1);
+  }
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+    .then ((response) => {
+      setData(response.data);
+    })
+    .catch((err) => console.error(err))
+  }, [])
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+    <StyledApp className="App">
+      <StyledHeader className="Header">Characters of The Star Wars Universe!</StyledHeader>
+      {
+        currentId && <Details data={data} currentId={currentId}/>
+      }
+      {
+        data.map((data, idx) => {
+          return <Character id={idx} openFunction={opener} data={data} />
+        })
+      }
+    </StyledApp>
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,20 @@
 // Write your Character component here
+import styled from "styled-components";
+
+const StyledCharacter = styled.div `
+    font-family: Fantasy;
+    padding: 1px;
+    margin: 10px;
+    background-color: rgba(255, 255, 255, 0.3);
+    margin-right: 300px;
+    margin-left: 300px;
+`
+
+export default function Character({ data, openFunction, id }) {
+   
+    return (
+        <StyledCharacter onClick={() => openFunction(id)} id={data.id}>
+            <h1>{data.name}</h1>
+        </StyledCharacter>
+    )
+}
